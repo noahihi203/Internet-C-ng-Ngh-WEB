638d56b8e9dc624b5f3aa9fae0a81be2bb9a2bf5
Vu Quang Huy <vuquanghuy0805@gmail.com>
2024-10-12 00:02:38 +0700

    fixed model 2

--- server/src/model/product.js
+++ server/src/model/product.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   Product.init({
     name: DataTypes.STRING,
     price: DataTypes.INTEGER,
-    type_of_clothes: DataTypes.ENUM('Shirt', 'Pants', 'Accessories'),
+    type_of_clothes: DataTypes.ENUM('Tees', 'Sweats', 'Shirts', 'Jeans', 'Shorts', 'Jackets', 'Sneakers', 'Sandals', 'Boxers', 'Socks', 'Hats', 'Accessories'),
     description: DataTypes.TEXT,
     cat_id: DataTypes.STRING,
     size: DataTypes.ENUM('S', 'M', 'L', '39', '40', '41', '42'),
